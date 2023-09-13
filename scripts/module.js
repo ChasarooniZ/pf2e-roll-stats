d904c8a46fccb7264ae7c4ea3c5b114030a3cc5d
cadowtin <chasdowtin@gmail.com>
2023-09-12 19:57:08 -0500

    comment out unused function

--- scripts/module.js
+++ scripts/module.js
@@ -74,67 +74,67 @@ export function generateStat(msg) {
     return res;
 }
 
-export function extractTerm(term, flavor = '') {
-    let result = [];
-    switch (term.constructor.name) {
-        case 'InstancePool':
-            for (const roll of term.rolls) {
-                result = result.concat(extractTerm(roll, term.flavor || flavor));
-            }
-            break;
-        case 'DamageInstance':
-            for (const item of term.terms) {
-                result = result.concat(extractTerm(item, term.types || flavor));
-            }
-            const keepPersistent = !!term.options.evaluatePersistent;
-            result = result
-                .filter(res => res.type.startsWith('persistent,') ? keepPersistent : true)
-                .map(r => ({ value: r.value, type: r.type.replace(/persistent,/g, '') }))
-            break;
-        case 'Grouping':
-            result = result.concat(extractTerm(term.term, term.flavor || flavor));
-            break;
-        case 'ArithmeticExpression':
-            switch (term.operator) {
-                case '+':
-                    for (const op of term.operands) {
-                        result = result.concat(extractTerm(op, term.flavor || flavor));
-                    }
-                    break;
-                case '-':
-                    result = result.concat(extractTerm(term.operands[0], term.flavor || flavor));
-                    result = result.concat(extractTerm(term.operands[1], term.flavor || flavor)).map(t => { return { value: -t.value, type: t.type } });
-                case '*':
-                    if (['NumericTerm', 'Die'].includes(term.operands[0].constructor.name)) {
-                        result = result.concat(extractTerm(term.operands[1], term.flavor || flavor).flatMap(i => [i, i]));
-                    } else if (['NumericTerm', 'Die'].includes(term.operands[1].constructor.name)) {
-                        result = result.concat(extractTerm(term.operands[0], term.flavor || flavor).flatMap(i => [i, i]));
-                    } else {
-                        result.push({ value: term.total, type: term.flavor || flavor })
-                    }
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case 'Die':
-            for (const dice of term.results) {
-                result.push({ value: dice.result, type: term.flavor || flavor })
-            }
-            break;
-        case 'NumericTerm':
-            result.push({ value: term.number, type: term.flavor || flavor })
-            break;
+// export function extractTerm(term, flavor = '') {
+//     let result = [];
+//     switch (term.constructor.name) {
+//         case 'InstancePool':
+//             for (const roll of term.rolls) {
+//                 result = result.concat(extractTerm(roll, term.flavor || flavor));
+//             }
+//             break;
+//         case 'DamageInstance':
+//             for (const item of term.terms) {
+//                 result = result.concat(extractTerm(item, term.types || flavor));
+//             }
+//             const keepPersistent = !!term.options.evaluatePersistent;
+//             result = result
+//                 .filter(res => res.type.startsWith('persistent,') ? keepPersistent : true)
+//                 .map(r => ({ value: r.value, type: r.type.replace(/persistent,/g, '') }))
+//             break;
+//         case 'Grouping':
+//             result = result.concat(extractTerm(term.term, term.flavor || flavor));
+//             break;
+//         case 'ArithmeticExpression':
+//             switch (term.operator) {
+//                 case '+':
+//                     for (const op of term.operands) {
+//                         result = result.concat(extractTerm(op, term.flavor || flavor));
+//                     }
+//                     break;
+//                 case '-':
+//                     result = result.concat(extractTerm(term.operands[0], term.flavor || flavor));
+//                     result = result.concat(extractTerm(term.operands[1], term.flavor || flavor)).map(t => { return { value: -t.value, type: t.type } });
+//                 case '*':
+//                     if (['NumericTerm', 'Die'].includes(term.operands[0].constructor.name)) {
+//                         result = result.concat(extractTerm(term.operands[1], term.flavor || flavor).flatMap(i => [i, i]));
+//                     } else if (['NumericTerm', 'Die'].includes(term.operands[1].constructor.name)) {
+//                         result = result.concat(extractTerm(term.operands[0], term.flavor || flavor).flatMap(i => [i, i]));
+//                     } else {
+//                         result.push({ value: term.total, type: term.flavor || flavor })
+//                     }
+//                     break;
+//                 default:
+//                     break;
+//             }
+//             break;
+//         case 'Die':
+//             for (const dice of term.results) {
+//                 result.push({ value: dice.result, type: term.flavor || flavor })
+//             }
+//             break;
+//         case 'NumericTerm':
+//             result.push({ value: term.number, type: term.flavor || flavor })
+//             break;
 
-        default:
-            console.error("Unrecognized Term when extracting parts", term)
-            result.push({ value: term.total, type: term.flavor || flavor })
-            break;
-    }
-    debugLog({ type: term.constructor.name, result }, 'extractTerm')
+//         default:
+//             console.error("Unrecognized Term when extracting parts", term)
+//             result.push({ value: term.total, type: term.flavor || flavor })
+//             break;
+//     }
+//     debugLog({ type: term.constructor.name, result }, 'extractTerm')
 
-    return result;
-}
+//     return result;
+// }
 
 export function debugLog(data, context = "") {
     if (game.settings.get("pf2e-roll-stats", 'debug-mode'))
