23d63b5dda8a66eae17afacab67e18a2ba59a51d
cadowtin <chasdowtin@gmail.com>
2024-03-18 12:48:59 -0500

    Fixed layer

--- scripts/module.js
+++ scripts/module.js
@@ -6,7 +6,7 @@ Hooks.on("init", () => {
   Hooks.on("getSceneControlButtons", async (controls) => {
     controls.push({
       icon: "fas fa-chart-simple",
-      layer: "token",
+      layer: "tokens",
       name: "rollstats",
       title: `PF2e Roll Stats`,
       visible: game.user.isGM,
